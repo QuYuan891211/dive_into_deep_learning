0083b0249da97ecc3475738282f16aa092ed94b0
qy <quyuan891211@163.com>
2022-09-29 16:27:15 +0800

    完成2.2

--- Day2/ch2.py
+++ Day2/ch2.py
@@ -1,6 +1,6 @@
 import torch
 import os
-
+import pandas as pd
 def book_ch2_1():
     x = torch.arange(12)
     print('2.1.1 获取一个张量（vector）')
@@ -49,18 +49,51 @@ def book_ch2_1():
     x[0:2,:] = 12
     print(x)
     print(y)
-
+    # [TODO]-2.1.5,2.1.6
     print('2.1.5 节省内存')
 
 def book_ch2_2():
+    print('2.2.1 数据预处理')
     #创建目录，存储CSV文件，如果目录不存在，则创建
     os.makedirs(os.path.join('/opt/data/dive_into_deep_learning', 'data'), exist_ok = True)
-    data_file = os.path.
+    data_file = os.path.join('/opt/data/dive_into_deep_learning', 'data', 'house_tiny.csv')
+    with open(data_file, 'w') as f:
+        #按行写入
+        f.write('NumRooms, Alley, Price\n') #列名
+        f.write('NA,Pave,127500\n')
+        f.write('2,NA,106000\n')
+        f.write('4,NA,178100\n')
+        f.write('NA,NA,140000\n')
 
+    #利用pandas读取CSV文件的内容
+    data = pd.read_csv(data_file)
+    print(data)
+    print('2.2.2 处理缺失值')
+    print('插值法处理缺失值')
+    inputs, outputs = data.iloc[:,0:2],data.iloc[:,2]
+    #均值处理NumRooms
+    inputs = inputs.fillna(inputs.mean())
+    print(inputs)
+    #将NaN视为一个类
+    inputs = pd.get_dummies(inputs,dummy_na = True)
+    print(inputs)
+
+    print('2.2.3 转换为张量格式')
+    tensor1,tensor2 = torch.tensor(inputs.values),torch.tensor(outputs.values)
+    print(torch.ones((2,3)))
+    print(tensor1)
+    print(tensor2)
+
+    print('homework:将预处理后的数据集转化为张量')
+    tensor2 = torch.reshape(tensor2,(4,1))
+    print(tensor2)
+    data_result = torch.cat((tensor1,tensor2),dim = 1)
+    print(data_result)
 def main():
     print("Hello World!")
 
 
 if __name__ == "__main__":
     main()
-    book_ch2_1()
+    #book_ch2_1()
+    book_ch2_2()
