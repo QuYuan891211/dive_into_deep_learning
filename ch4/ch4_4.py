ef1b5dbe3cad7bc59486adf8c0d190156620b035
quy <quyuankenny@163.com>
2023-02-13 15:58:00 +0800

    完成：CH4.4 模型过拟合 欠拟合

--- ch4/ch4_4.py
+++ ch4/ch4_4.py
@@ -5,14 +5,19 @@ from d2l import torch as d2l
 from torch import nn
 from matplotlib import pyplot as plt
 
+
 def main():
     print('ch4_4 过拟合和欠拟合')
+    # 1. 设定一个权重矩阵（1维）
     max_degree = 20  # 多项式的最大阶数
     n_train, n_test = 100, 100  # 训练和测试数据集大小
     true_w = np.zeros(max_degree)  # 分配大量的空间
+    # 只有前4个W是有用的，其余全是0
     true_w[0:4] = np.array([5, 1.2, -3.4, 5.6])
 
+    # 2. 随机产生正态分布的函数，进行采样，获得数据集（训练集+测试集）合计的长度。
     features = np.random.normal(size=(n_train + n_test, 1))
+    # 打乱feature的顺序
     np.random.shuffle(features)
 
     poly_features = np.power(features, np.arange(max_degree).reshape(1, -1))
@@ -38,7 +43,8 @@ def main():
     train(poly_features[:n_train, :], poly_features[n_train:, :],
           labels[:n_train], labels[n_train:], num_epochs=1500)
 
-def evaluate_loss(net, data_iter, loss):  #@save
+
+def evaluate_loss(net, data_iter, loss):  # @save
     """评估给定数据集上模型的损失"""
     metric = d2l.Accumulator(2)  # 损失的总和,样本数量
     for X, y in data_iter:
@@ -48,6 +54,7 @@ def evaluate_loss(net, data_iter, loss):  #@save
         metric.add(l.sum(), l.numel())
     return metric[0] / metric[1]
 
+
 def train(train_features, test_features, train_labels, test_labels,
           num_epochs=400):
     loss = nn.MSELoss(reduction='none')
@@ -55,9 +62,9 @@ def train(train_features, test_features, train_labels, test_labels,
     # 不设置偏置，因为我们已经在多项式中实现了它
     net = nn.Sequential(nn.Linear(input_shape, 1, bias=False))
     batch_size = min(10, train_labels.shape[0])
-    train_iter = d2l.load_array((train_features, train_labels.reshape(-1,1)),
+    train_iter = d2l.load_array((train_features, train_labels.reshape(-1, 1)),
                                 batch_size)
-    test_iter = d2l.load_array((test_features, test_labels.reshape(-1,1)),
+    test_iter = d2l.load_array((test_features, test_labels.reshape(-1, 1)),
                                batch_size, is_train=False)
     trainer = torch.optim.SGD(net.parameters(), lr=0.01)
     animator = d2l.Animator(xlabel='epoch', ylabel='loss', yscale='log',
